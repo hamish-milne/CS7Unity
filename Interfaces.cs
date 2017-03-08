6ef293c600feed67ddfea98ea621cc75ca411521
hamish-milne <hamishmilne83@gmail.com>
2017-03-08 02:11:10 +0000

    v1.0

---
+++ Interfaces.cs
@@ -0,0 +1,13 @@
+﻿namespace System.Collections
+{
+	public interface IStructuralEquatable
+	{
+		bool Equals(object other, IEqualityComparer comparer);
+		int GetHashCode(IEqualityComparer comparer);
+	}
+
+	public interface IStructuralComparable
+	{
+		int CompareTo(object other, IComparer comparer);
+	}
+}
